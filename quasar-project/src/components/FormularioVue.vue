b97cde33e9617fff615cf18ae08895ff4169c1c5
gabriel <gabrielcomputacao@outlook.com>
2023-04-16 11:34:43 -0300

    (refactor)aplicação: organização da aplicação e primeiro commit de pastas

---
+++ quasar-project/src/components/FormularioVue.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="q-pa-md" style="min-width: 500px ;">
+    <q-form>
+      <div class="row justify-center">
+        <q-input class="inputText" filled v-model="textoBusca" label="Digite o nome ou email" />
+        <q-btn label="Submit" type="submit" color="primary" />
+
+      </div>
+    </q-form>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+const textoBusca = ref('')
+export default defineComponent({
+  setup () {
+    return {
+      textoBusca
+    }
+  }
+})
+</script>
+
+<style scoped>
+  .inputText{
+    width: 100%;
+    margin-bottom: 1rem;
+  }
+
+</style>
---
+++ quasar-project/src/pages/ManagementPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="column items-center justify-center">
+    <div class="bg-light-blue-4" style="width: 400px;height: auto;">
+      <q-img src="../assets/img/onfly-header.svg" />
+    </div>
+    <h3>Gestão de Usuários</h3>
+    <div>
+      <FormularioVue />
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import FormularioVue from 'src/components/FormularioVue.vue'
+</script>
